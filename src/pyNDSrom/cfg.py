b427a1cc097dcdeb2763e17a63ec2767e8276554
Artem Savkov <sm00th.trac3r@gmail.com>
2011-06-04 14:31:30 +0400

    config stuff moved to separate file, still just a dict though

---
+++ src/pyNDSrom/cfg.py
@@ -0,0 +1,20 @@
+import os
+
+config = {
+    'confDir' : os.path.expanduser( "~/.pyROManager" ),
+    'dbFile'  : 'pyro.db',
+    'xmlDB'   : 'ADVANsCEne_NDS_S.xml',
+    'location' : {
+        0  : ( 'Europe'      , 'EUR'   , 'E' ),
+        1  : ( 'USA'         , 'USA'   , 'U' ),
+        2  : ( 'Germany'     , 'GER'   , 'G' ),
+        4  : ( 'Spain'       , 'SPA'   , 'S' ),
+        5  : ( 'France'      , 'FRA'   , 'F' ),
+        6  : ( 'Italy'       , 'ITA'   , 'I' ),
+        7  : ( 'Japan'       , 'JPN'   , 'J' ),
+        8  : ( 'Netherlands' , 'DUTCH' , 'N' ),
+        19 : ( 'Australia'   , 'AUS'   , 'A' ),
+        22 : ( 'Korea'       , 'KOR'   , 'K' ),
+    },
+}
+

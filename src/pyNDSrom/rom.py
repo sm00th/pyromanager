d8f4515aeaf750754547d8f00fd3b4c9235c0785
Artem Savkov <sm00th.trac3r@gmail.com>
2011-06-19 23:56:49 +0400

    moving methods around

--- src/pyNDSrom/rom.py
+++ src/pyNDSrom/rom.py
@@ -5,18 +5,37 @@ import cfg
 
 class RomInfo:
     '''Rom info'''
-    def __init__( self, release_id = None ):
-        pass
+    def __init__( self, database, release_id = None ):
+        self.database = database
 
     def __str__( self ):
         pass
 
 class FileInfo:
     '''File info'''
-    def __init__( self, path, database ):
-        self.database = database
-        self.path     = path
-        self.info     = {}
+    def __init__( self, path, database, config ):
+        self.database  = database
+        self.config    = config
+        self.path      = path
+        self.info      = {}
+        self.name_info = None
+
+    def init( self ):
+        try:
+            # FIXME: helloooo, archives
+            self.info['size'] = os.path.getsize( self.path )
+
+            handler          = open( self.path, 'rb' )
+            self.info['crc'] = binascii.crc32( handler.read() & 0xFFFFFFFF )
+            handler.close()
+        except IOError, OSError as exc:
+            print 'Failed to read file %s: %s' % ( self.file_path, exc )
+
+    def query_db( self ):
+        if not self.info:
+            self.init()
+
+        relid = self.database.search_known_crc( self.info['crc'] )
 
     def split_path( self ):
         archive_path = self.path
@@ -33,8 +52,56 @@ class FileInfo:
             result = 1
         return result
 
-    def query_db( self ):
-        pass
+    def filename( self ):
+        '''Formatted filename'''
+        # TODO: configurable filename format
+        if self.rom_info['release_number'] or self.rom_info['name']:
+            name = "%04d - %s (%s).nds" % (
+                int( self.rom_info['release_number'] ), self.rom_info['name'],
+                    self.rom_info['region'] )
+        else:
+            archive_file = self.archive_path()[1]
+            if archive_file:
+                name = os.path.basename( archive_file )
+            else:
+                name = os.path.basename( self.file_info['path'] )
+
+        return name
+
+    def upload( self, path ):
+        '''Copy rom to flashcart'''
+        ( main_path, archive_file ) = self.archive_path()
+        if not archive_file:
+            shutil.copy( main_path, '%s/%s' % ( path, self.filename() ) )
+        else:
+            ext  = pyNDSrom.file.extension( main_path )
+            if ext == 'zip':
+                archive = pyNDSrom.file.ZIP( main_path )
+            elif ext == '7z':
+                archive = pyNDSrom.file.ZIP7( main_path )
+            elif ext == 'rar':
+                archive = pyNDSrom.file.RAR( main_path )
+            archive.extract( archive_file, path )
+            os.rename( '%s/%s' % ( path, archive_file ),
+                    '%s/%s' % ( path, self.filename() ) )
+
+    def size_mb( self ):
+        '''Returns size in MB'''
+        result = 'Unknown'
+        if self.file_info['size']:
+            result = "%.2fM" % ( self.file_info['size'] / 1048576.0 )
+        return result
+
+    def remove( self ):
+        '''Remove file from disk and local_roms table'''
+        path = re.sub( r":.*$", '', self.file_info['path'] )
+        try:
+            os.unlink( path )
+            self.database.remove_local( path )
+        except OSError as exc:
+            print "Failed to remove file: %s" % ( exc )
+
+        return 1
 
     def __str__( self ):
         pass
@@ -76,94 +143,6 @@ class Rom:
                 self.normalized_name, self.file_info['size'],
                 self.file_info['crc32'] )
         return dataset
-
-    def set_file_info( self, file_data ):
-        '''Set file information'''
-        self.file_info = {
-                'path'  : file_data[0],
-                'size'  : file_data[1],
-                'crc32' : file_data[2],
-        }
-        return 1
-
-    def set_rom_info( self, rom_data ):
-        '''Set rom information'''
-        config = cfg.Config()
-        config.read_config()
-        self.rom_info = {
-                'release_number'  : rom_data[0],
-                'name'            : rom_data[1],
-                'publisher'       : rom_data[2],
-                'released_by'     : rom_data[3],
-                'region'          : config.region_name( rom_data[4] ),
-                'normalized_name' : rom_data[5],
-        }
-        return 1,
-
-    def size_mb( self ):
-        '''Returns size in MB'''
-        result = 'Unknown'
-        if self.file_info['size']:
-            result = "%.2fM" % ( self.file_info['size'] / 1048576.0 )
-        return result
-
-
-    def archive_path( self ):
-        '''Separates inarchive file from actual path'''
-        main_path    = self.file_info['path']
-        archive_file = None
-        try:
-            ( main_path, archive_file ) = self.file_info['path'].split( ':' )
-        except ValueError:
-            pass
-        return ( main_path, archive_file )
-
-    def filename( self ):
-        '''Formatted filename'''
-        # TODO: filename format
-        if self.rom_info['release_number'] or self.rom_info['name']:
-            name = "%04d - %s (%s).nds" % (
-                int( self.rom_info['release_number'] ), self.rom_info['name'],
-                    self.rom_info['region'] )
-        else:
-            archive_file = self.archive_path()[1]
-            if archive_file:
-                name = os.path.basename( archive_file )
-            else:
-                name = os.path.basename( self.file_info['path'] )
-
-        return name
-
-    def upload( self, path ):
-        '''Copy rom to flashcart'''
-        ( main_path, archive_file ) = self.archive_path()
-        if not archive_file:
-            shutil.copy( main_path, '%s/%s' % ( path, self.filename() ) )
-        else:
-            ext  = pyNDSrom.file.extension( main_path )
-            if ext == 'zip':
-                archive = pyNDSrom.file.ZIP( main_path )
-            elif ext == '7z':
-                archive = pyNDSrom.file.ZIP7( main_path )
-            elif ext == 'rar':
-                archive = pyNDSrom.file.RAR( main_path )
-            archive.extract( archive_file, path )
-            os.rename( '%s/%s' % ( path, archive_file ),
-                    '%s/%s' % ( path, self.filename() ) )
-
-    def remove( self, database = None ):
-        '''Remove file from disk, if database is specified also remove from
-        local_roms'''
-        path = re.sub( r":.*$", '', self.file_info['path'] )
-        try:
-            os.unlink( path )
-        except OSError as exc:
-            print "Failed to remove file: %s" % ( exc )
-
-        if database:
-            database.remove_local( path )
-
-        return 1
 
     def __str__( self ):
         # TODO: fix formatting somehow

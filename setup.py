48f8657c7318d9c2e2de93c5040fc79724761c1d
Artem Savkov <artem.savkov@gmail.com>
2011-07-07 12:09:00 +0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name          = "pyromanager",
-        version      = "0.1.0",
+        version      = "0.1.1",
         description  = 'nds rom manager',
         author       = 'Artem Savkov',
         author_email = 'artem.savkov@gmail.com',
